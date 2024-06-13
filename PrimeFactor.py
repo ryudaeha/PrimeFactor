b8efdfc46e3eba085874e8276792e6d6d255022c
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-13 09:13:40 +0900

    first

---
+++ PrimeFactor.py
@@ -0,0 +1,3 @@
+class PrimeFactor:
+    1
+    pass
---
+++ test_PrmieFactor.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from PrimeFactor import PrimeFactor
+
+
+class PrimeFactorTest(TestCase):
+    def test_prime_factor(self):
+        pf = PrimeFactor()
+        self.assertEqual(1,1)
